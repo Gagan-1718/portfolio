4e2cfa9277444c40006a7903b5fa95634441ee87
Gagan-1718 <jsspshsr.ixc.gagandeep@gmail.com>
2025-12-26 21:53:17 +0530

    updatedd

--- project/JS/main.js
+++ project/JS/main.js
@@ -1,53 +1,69 @@
-let menuIcon=document.querySelector('#menu-icon');
-let navBar=document.querySelector('.navbar');
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
 
-menuIcon.onclick=()=>{
-    menuIcon.classList.toggle('fa-mark');
-    navBar.classList.togggle('active');
-}
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('fa-bars');
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+};
 
-let sections=document.querySelector('section');
-let navLinks=document.querySelector('header nav a');
 
-window.onscroll=()=>{
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
     sections.forEach(sec => {
-        let top=window.scrollY;
-        let offset=sec.offsetTop-150;
-        let height=sec.offsetHeight;
-        let id=sec.getAtrribute('id');
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
 
-        if(top>=offset && top<offset+height){
-            navLinks.forEach.apply(links=>{
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*='+id+']').classList.add('active');
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
             });
-        };
+
+            document
+              .querySelector('header nav a[href*="' + id + '"]')
+              .classList.add('active');
+        }
     });
 
-    let header=document.querySelector('header');
-    header.classList.toggle('sticky',window.scrollY>100);
 
+    let header = document.querySelector('.header');
+    header.classList.toggle('sticky', top > 100);
+
+
+    menuIcon.classList.add('fa-bars');
     menuIcon.classList.remove('fa-xmark');
-    navBar.classList.remove('active');
-}
+    navbar.classList.remove('active');
+};
 
-ScrollReveal({
-    distance:'80px',
-    duration: 2000,
-    delay:200,
-}); 
 
-ScrollReveal().reveal('.home-content,heading', {origin:'top'});
-ScrollReveal().reveal('.home-img,.services-container, .portfolio-box,.contact form', {origin:'bottom'});
-ScrollReveal().reveal('.home-contact h1,about-img', {origin:'left'});
-ScrollReveal().reveal('.home-contact p,.about-content', {origin:'right'});
-
-const typed=new Typed('.mulitple-text',{
-    strings: ['AIML Engineer', 'Tech Enthusiast','Tabla Artist'],
-    typeSpeed:50,
-    backSpeed:50,
-    backDelay:1000,
-    loop:true,
-
+navLinks.forEach(link => {
+    link.onclick = () => {
+        menuIcon.classList.add('fa-bars');
+        menuIcon.classList.remove('fa-xmark');
+        navbar.classList.remove('active');
+    };
 });
 
+ScrollReveal({ 
+    distance : '80px',
+    duration : 2000,
+    delay : 200
+ });
+
+ScrollReveal().reveal('.home-content, heading', { origin : 'top'});
+ScrollReveal().reveal('.home-img, .portfolio-box, .contact form', { origin : 'bottom' });
+ScrollReveal().reveal('.home-content h1, .about-img', { origin : 'left'});
+ScrollReveal().reveal('.home-content p, .about-content', { origin : 'right'});
+
+const typed = new Typed('.multiple-text', {
+    strings : ['AIML Engineer', 'Tech Enthusiast', 'Tabla Artist'],
+    typeSpeed : 50,
+    backSpeed : 50,
+    backDelay : 1000,
+    loop : true
+})
